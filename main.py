ddcbe9227a265da2d1b141498cae4e05e04d7b93
doug <drehnstrom@yahoo.com>
2019-08-10 10:06:04 -0400

    Changed Message

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    model = {"title":"Welcome to the Great Converter!"}
+    model = {"title":"Welcome to Converter!"}
     return render_template('index.html', model=model)
 
 @app.route("/temp-converter")
